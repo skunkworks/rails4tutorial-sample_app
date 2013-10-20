dbc2993d84e2a3715ef142d45beab57a43e59397
Richard Shin <skunkworks@gmail.com>
2013-10-20 12:16:33 -0700

    Added Guard gem

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -2,21 +2,28 @@ require 'spec_helper'
 
 feature "StaticPages" do
 
+	let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
 	scenario "User visits the Home page" do
 		visit '/static_pages/home'
 		expect(page).to have_text('Sample App')
-		expect(page).to have_title('Ruby on Rails Tutorial Sample App | Home')
+		expect(page).to have_title("#{base_title} | Home")
 	end
 
 	scenario "User visits the Help page" do
 		visit '/static_pages/help'
 		expect(page).to have_text('Help')
-		expect(page).to have_title('Ruby on Rails Tutorial Sample App | Help')
+		expect(page).to have_title("#{base_title} | Help")
 	end
 
 	scenario "User visits the About page" do
 		visit '/static_pages/about'
 		expect(page).to have_text('About Us')
-		expect(page).to have_title('Ruby on Rails Tutorial Sample App | About Us')
+		expect(page).to have_title("#{base_title} | About Us")
+	end
+
+	scenario "User visits the Contact page" do
+		visit '/static_pages/contact'
+		expect(page).to have_title("#{base_title} | Contact")
 	end
 end
