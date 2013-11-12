bac7328bc0c24843754458ec996f11e3e18d0c0b
Richard Shin <skunkworks@gmail.com>
2013-11-11 17:59:31 -0800

    Tests for welcome flash

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -41,6 +41,14 @@ describe "User pages" do
       it 'creates a new user' do
         expect{ click_button(submit) }.to change(User, :count).by(1)
       end
+
+      describe 'after saving the user' do
+        before { click_button(submit) }
+        let (:user) { User.find_by_email("example@example.com") }
+
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+      end
     end
   end
 end
