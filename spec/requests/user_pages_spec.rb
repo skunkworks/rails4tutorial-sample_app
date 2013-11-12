c419ae672a1498da938f42c22667dd6bc077b481
Richard Shin <skunkworks@gmail.com>
2013-11-11 16:15:50 -0800

    Finished user signup

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -12,11 +12,28 @@ describe "User pages" do
     it { should have_text(user.name) }
     it { should have_title(user.name) }
   end
-  
-  describe "signup page" do
-    before { visit signup_path }
 
-    it { should have_text('Sign up') }
-    it { should have_title(full_title('Sign up')) }
+  describe 'signup' do
+    before { visit signup_path }
+    let (:submit) { "Create my account" }
+
+    context 'with invalid information' do
+      it 'does not create new user' do
+        expect{ click_button(submit) }.not_to change(User, :count)
+      end
+    end
+
+    context 'with valid information' do
+      before do
+        fill_in 'Name', with: "Example User"
+        fill_in 'Email', with: "example@example.com"
+        fill_in 'Password', with: "foobar"
+        fill_in 'Confirm password', with: "foobar"
+      end
+
+      it 'creates a new user' do
+        expect{ click_button(submit) }.to change(User, :count).by(1)
+      end
+    end
   end
 end
