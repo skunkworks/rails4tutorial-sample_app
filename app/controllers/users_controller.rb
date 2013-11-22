e1b09ebd1b39df3484ce4d340ca12c9aca467e89
Richard Shin <skunkworks@gmail.com>
2013-11-22 11:16:27 -0800

    Add'l comment to clarify strong params feature in Rails 4

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,20 @@ class UsersController < ApplicationController
   end
 
   private
+
+  # Strong params -- anywhere you'd previously pass in params willy-nilly, replace with
+  # calls to a private method such as below.
+  #
+  # #require is an ActionController::Parameters instance method that looks at the parameters
+  # and requires that the given key exists. If it does, it returns the paramter at that key.
+  #
+  # #permit is also an ActionController::Parameters instance method. It takes in one or more
+  # attribute names as symbols and returns the params filtered to only those attributes.
+  #
+  # In this example, require returns the user hash, and permit filters that user hash such
+  # that it contains only the permitted attributes.
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
+
 end
